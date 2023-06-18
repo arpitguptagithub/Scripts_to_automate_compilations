e87c837a67dbf8db5b4f05e06e074f0aca3d529d
Arpit Gupta <98796025+arpitguptagithub@users.noreply.github.com>
2023-06-18 21:23:47 +0530

    First try. Just produce a.out
    Create a Source folder 
    Run the code where your "Source " folder is located.
    The output folder will be created there, and you can chech the output files

---
+++ script.py
@@ -0,0 +1,29 @@
+import os
+import subprocess
+import shutil
+
+def compile_files(folder_path, output_folder_path, compile_command):
+    os.makedirs(output_folder_path, exist_ok=True)
+    
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith('.c'):
+                file_path = os.path.join(root, file)
+                output_file = file.replace('.c', '.out')
+                output_subfolder = os.path.relpath(root, folder_path)
+                output_path = os.path.join(output_folder_path, output_subfolder, output_file)
+                os.makedirs(os.path.dirname(output_path), exist_ok=True)
+                subprocess.call([compile_command, file_path, '-o', output_path])
+
+# Example usage
+folder_name = 'source'
+output_folder_name = 'output'
+compile_command = 'gcc'  # Example compilation command, modify as per your requirements
+
+# Search for the 'codes' folder in the current working directory and get its path
+folder_path = os.path.join(os.getcwd(), folder_name)
+
+# Create the output folder path relative to the current working directory
+output_folder_path = os.path.join(os.getcwd(), output_folder_name)
+
+compile_files(folder_path, output_folder_path, compile_command)
